9d810f4aac2daa419f285f99ec4b3e30ec1067c4
firnassw <firnaswahid@gmail.com>
2026-09-25 23:00:24 +0700

    Fix: Update CSP to allow Google Fonts and Material Icons

--- next.config.ts
+++ next.config.ts
@@ -22,9 +22,9 @@ const nextConfig: NextConfig = {
     const cspHeader = `
       default-src 'self';
       script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live;
-      style-src 'self' 'unsafe-inline';
+      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
       img-src 'self' blob: data: https:;
-      font-src 'self' data:;
+      font-src 'self' data: https://fonts.gstatic.com;
       object-src 'none';
       base-uri 'self';
       form-action 'self';
