88d4a45b9bd5a3d4a1d38c7db1cdf1b887fef2e2
firnassw <firnaswahid@gmail.com>
2026-09-25 22:54:37 +0700

    Fix: Add external image domain to config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const withBundleAnalyzer = bundleAnalyzer({
 });
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+    ],
+  },
   async headers() {
     const cspHeader = `
       default-src 'self';
