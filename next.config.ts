6e017ab4cc4c94824eb92306dfd1cefcd5bce7e4
firnassw <firnaswahid@gmail.com>
2026-09-25 22:55:10 +0700

    Fix: Add mediascanter.id to image domains

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
       },
+      {
+        protocol: 'https',
+        hostname: 'mediascanter.id',
+      },
     ],
   },
   async headers() {
