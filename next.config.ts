71d0d2947c9e44b48747ef8dd7953dae6a892a24
firnassw <firnaswahid@gmail.com>
2026-09-25 21:26:01 +0700

    Restore images unoptimized and eslint ignore config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // Menginformasikan Turbopack lokasi root proyek
   turbopack: { root: __dirname },
 };
